7bae924a4ed0a3ba0038baf68536c78f575d20dd
Tofunmi Enitan <toffeegold16@gmail.com>
2024-11-17 03:06:28 +0100

    photo gallery

---
+++ script.js
@@ -0,0 +1,55 @@
+const change = document.getElementById("photo1");
+
+change.addEventListener("click", (event) => {
+    event.target.style.background = "gold";
+    event.target.textContent = "Black on yellow" ;
+});
+
+const change2 = document.getElementById("photo2");
+
+change2.addEventListener("click", (event) => {
+    event.target.style.background = "#f2f2f2";
+    event.target.textContent = "Hoodie on jean" ;
+});
+
+// const change2 = document.getElementById("photo2");
+
+// change2.addEventListener("click", (event) => {
+//     event.target.style.background = "#f2f2f2";
+//     event.target.textContent = "Hoodie on jean" ;
+// });
+
+// const change2 = document.getElementById("photo2");
+
+// change2.addEventListener("click", (event) => {
+//     event.target.style.background = "#f2f2f2";
+//     event.target.textContent = "Hoodie on jean" ;
+// });
+
+// const change2 = document.getElementById("photo2");
+
+// change2.addEventListener("click", (event) => {
+//     event.target.style.background = "#f2f2f2";
+//     event.target.textContent = "Hoodie on jean" ;
+// });
+
+// const change2 = document.getElementById("photo2");
+
+// change2.addEventListener("click", (event) => {
+//     event.target.style.background = "#f2f2f2";
+//     event.target.textContent = "Hoodie on jean" ;
+// });
+
+// const change2 = document.getElementById("photo2");
+
+// change2.addEventListener("click", (event) => {
+//     event.target.style.background = "#f2f2f2";
+//     event.target.textContent = "Hoodie on jean" ;
+// });
+
+// const change2 = document.getElementById("photo2");
+
+// change2.addEventListener("click", (event) => {
+//     event.target.style.background = "#f2f2f2";
+//     event.target.textContent = "Hoodie on jean" ;
+// });
